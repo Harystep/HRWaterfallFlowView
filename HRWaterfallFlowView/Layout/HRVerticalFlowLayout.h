26eb4e82a1a026f49bb8a58521a67ef7a68996a6
Harystep <warm588@163.com>
2024-02-02 09:17:36 +0800

    first commit

---
+++ HRWaterfallFlowView/Layout/HRVerticalFlowLayout.h
@@ -0,0 +1,16 @@
+//
+//  HRVerticalFlowLayout.h
+//  HRWaterfallFlowView
+//
+//  Created by oneStep on 2024/2/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HRVerticalFlowLayout : UICollectionViewLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HRWaterfallFlowView/Layout/HRVerticalFlowLayout.m
@@ -0,0 +1,12 @@
+//
+//  HRVerticalFlowLayout.m
+//  HRWaterfallFlowView
+//
+//  Created by oneStep on 2024/2/2.
+//
+
+#import "HRVerticalFlowLayout.h"
+
+@implementation HRVerticalFlowLayout
+
+@end
